199a805b92adde3adeef06122fa175c1428184fd
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-12 05:32:16 +0500

    mathmatic
    mathmatic in typescript

---
+++ main.ts
@@ -0,0 +1,52 @@
+/*//Arithmatice
+//addition
+console.log(10+10);
+
+let num1=20;
+let num2=10;
+let num3='5'
+let num4=20;
+let sum = num1+num2;
+console.log(sum);
+//subtrction:
+
+let difference = num1-num2;
+console.log(difference);
+
+//multiplication
+let product = num1*num2;
+ console.log(product);
+
+ //division
+ let quotient=num1/num2;
+ console.log(quotient);
+
+ //reminder
+ //let reminder=num1%num2;
+ //console.log(reminder);
+
+//common
+console.log((200-100)*10/2);
+
+//module (%) reminder
+let reminder=num1%num2;
+console.log(reminder)*/
+
+// Relationl /Comparison == ,===,< ,<=,>,>=,
+
+let num1=20;
+let num2=10;
+let num3='5';
+let num4=20;
+//equal
+
+console.log(num1==num2);
+console.log(num1===num2);
+//less then
+console.log(num1<num2);
+//less then equal
+console.log(num1<=num2);
+//greater then
+console.log(num1>num2);
+//greter then equal to
+console.log(num1>=num2);
